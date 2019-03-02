f4e8ce360b1ed1080169d9ba98d24406d76747b2
lelopez <ux.lelopez@gmail.com>
2019-03-02 15:43:42 -0600

    add catalog section to movies

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="movies">
     <Featured />
+    <Catalog />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Featured from '@/components/layout/Featured.vue'
+import Catalog from '@/components/Catalog.vue'
 
 export default {
   name: 'movies',
   components: {
-    Featured
+    Featured,
+    Catalog
   }
 }
 </script>
