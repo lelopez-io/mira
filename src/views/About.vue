f45971ffe5fb58d3fd6ef016455aa4afbabc3ce7
lelopez <ux.lelopez@gmail.com>
2019-03-02 12:53:56 -0600

    add an about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About MIRA</h1>
+    <h2>MIRA aims at bringing social features to Netflix</h2>
+    <p>
+      Mira is the spanish word for look, as in look at what your friends are
+      watching. The idea is to see what your friends are watching, have watched,
+      or stopped watching while browsing through titles. This idea could be
+      extended to include messages both public and private with the option to
+      filter out possible spoilers.
+    </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  padding: 30px;
+
+  & h1 {
+    margin-bottom: 0;
+  }
+
+  & h2 {
+    margin-top: 0;
+    color: #e50914;
+  }
+}
+</style>
