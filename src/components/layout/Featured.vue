4b7ea805520ba5b6473639227531faa276977b98
lelopez <ux.lelopez@gmail.com>
2019-03-02 15:41:16 -0600

    set up featured section

--- src/components/layout/Featured.vue
+++ src/components/layout/Featured.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="featured">
+    <video
+      class="video"
+      playsinline=""
+      autoplay=""
+      muted=""
+      loop=""
+      src="https://app.coverr.co/s3/mp4/Magic-Mouse-Scroll.mp4"
+      poster="https://storage.googleapis.com/coverr-public/poster/Magic-Mouse-Scroll.jpg"
+    ></video>
+
     <div class="selection" v-if="selection !== ''">
       <h1>{{ selection }}</h1>
       <select>
@@ -57,7 +67,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#featured {
+  box-shadow: inset 0px 0px 150px 60px rgba(0, 0, 0, 0.8);
+
+  z-index: -1;
+  width: 100%;
+  height: calc(95vh - 100px);
+}
 .selection {
+  color: #fff;
   padding: 30px;
   display: flex;
   align-items: center;
@@ -74,5 +92,25 @@ export default {
     line-height: 2rem;
     font-size: 0.9rem;
   }
+}
+
+.video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: -1000;
+  overflow: hidden;
+}
+
+.selection {
+  position: absolute;
+  top: 100px;
+  left: 0;
 }
 </style>
