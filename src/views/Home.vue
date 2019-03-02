e3c883bdf30b39997b663578bde6eaf22f7d5444
lelopez <ux.lelopez@gmail.com>
2019-03-02 15:42:08 -0600

    add catalog section to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Featured />
+    <Catalog />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Featured from '@/components/layout/Featured.vue'
+import Catalog from '@/components/Catalog.vue'
 
 export default {
   name: 'home',
   components: {
-    Featured
+    Featured,
+    Catalog
   }
 }
 </script>
