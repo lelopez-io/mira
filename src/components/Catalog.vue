c180c03a412b024124f5ecd2e198780207025535
lelopez <ux.lelopez@gmail.com>
2019-03-03 14:06:17 -0600

    add a list of friends to catalog control

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -12,6 +12,7 @@
             return title.id === selected
           })
         "
+        :friendsList="friendsList"
       />
     </div>
   </div>
@@ -56,7 +57,72 @@ export default {
           price: 19.99
         }
       ],
-      selected: undefined
+      selected: undefined,
+      friendsList: [
+        {
+          id: 1,
+          name: 'luis',
+          percentWatched: 33,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 2,
+          name: 'enrrique',
+          percentWatched: 53,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 3,
+          name: 'lopez',
+          percentWatched: 93,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 4,
+          name: 'leon',
+          percentWatched: 100,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 5,
+          name: 'lelopez',
+          percentWatched: 13,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 6,
+          name: 'enrrique',
+          percentWatched: 53,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 7,
+          name: 'lopez',
+          percentWatched: 93,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 8,
+          name: 'leon',
+          percentWatched: 100,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        },
+        {
+          id: 9,
+          name: 'lelopez',
+          percentWatched: 13,
+          img:
+            'https://pbs.twimg.com/profile_images/1072569190924607489/kUIUy96j_400x400.jpg'
+        }
+      ]
     }
   },
   methods: {
@@ -66,6 +132,7 @@ export default {
       if (element) {
         element.scrollIntoView({ behavior: 'smooth', block: 'end' })
       }
+      //get list of friends watching this selected title and sort by highest
     }
   }
 }
